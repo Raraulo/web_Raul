500a624e29d6d6fe12fd3c967cca422d8f36804f
Raraulo <reamaguana@puce.edu.ec>
2026-07-07 20:27:36 -0500

    Agregar Microsoft Clarity

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
+import Clarity from '@microsoft/clarity'
 import './index.css'
 import App from './App.jsx'
+
+Clarity.init("xizus3zlmv")
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
